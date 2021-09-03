9ed0ff808200eca4bcd7be51831f334374ae948e
Jaha  Naeem Gitonga <jahagitonga@Jahas-MacBook-Pro.local>
2021-09-02 22:09:12 -0400

    feat: Add ability to sort shows

--- pages/shows.js
+++ pages/shows.js
@@ -1,26 +1,61 @@
-import Layout from '@c/Layout'
-import { Grid, Card } from '@c/Grid'
-import { Title } from '@c/Title'
-import { getAllShows } from '@l/graphcms'
+import { useEffect, useState } from 'react';
+import Layout from '@c/Layout';
+import { Grid, Card } from '@c/Grid';
+import { Title } from '@c/Title';
+import { getAllShows } from '@l/graphcms';
+import SortingBar from '@c/SortingBar';
 
 export default function Shows({ shows }) {
+  const [sortedShows, setSortedShows] = useState([...shows]);
+  const handleSort = (order, name) => {
+    if (name.includes('name')) {
+      order === 'asc' &&
+        setSortedShows([
+          ...shows.sort((a, b) => a.title.localeCompare(b.title)),
+        ]);
+      order === 'desc' &&
+        setSortedShows([
+          ...shows.sort((a, b) => b.title.localeCompare(a.title)),
+        ]);
+      return;
+    }
+
+    order === 'asc' &&
+      setSortedShows([
+        ...shows.sort(
+          (a, b) =>
+            new Date(a.scheduledStartTime) - new Date(b.scheduledStartTime)
+        ),
+      ]);
+    order === 'desc' &&
+      setSortedShows([
+        ...shows.sort(
+          (a, b) =>
+            new Date(b.scheduledStartTime) - new Date(a.scheduledStartTime)
+        ),
+      ]);
+  };
+
   return (
-    <Layout title="next-graphcms-shows / Shows">
+    <Layout title='next-graphcms-shows / Shows'>
       <Title>Shows</Title>
       <Grid>
-        {shows.map(show => (
+        <SortingBar setSortOrder={handleSort} />
+      </Grid>
+      <Grid>
+        {sortedShows.map((show) => (
           <Card href={`/show/${show.slug}`} header={show.title} key={show.id}>
             <p>{show.artists.map(({ fullName }) => fullName).join(', ')}</p>
           </Card>
         ))}
       </Grid>
     </Layout>
-  )
+  );
 }
 
 export async function getServerSideProps() {
-  const shows = (await getAllShows()) || []
+  const shows = (await getAllShows()) || [];
   return {
     props: { shows },
-  }
+  };
 }
